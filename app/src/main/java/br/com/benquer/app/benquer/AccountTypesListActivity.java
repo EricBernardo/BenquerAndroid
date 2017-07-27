16b3807f6d521e28a053bed99c892d2b77e71e23
Eric Bernardo <eric.sousa@leadforce.com.br>
2017-07-27 16:43:06 -0300

    Create AccountTypesListActivity

--- app/src/main/java/br/com/benquer/app/benquer/AccountTypesListActivity.java
+++ app/src/main/java/br/com/benquer/app/benquer/AccountTypesListActivity.java
@@ -1,13 +1,23 @@
 package br.com.benquer.app.benquer;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class AccountTypesListActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_types_list);
+
+        sharedPref = getSharedPreferences("Auth", Context.MODE_PRIVATE);
+        String access_token = sharedPref.getString("access_token", "");
+        Log.d("AccountTypesListActivity", access_token);
+
     }
 }
--- app/src/main/java/br/com/benquer/app/benquer/LoginActivity.java
+++ app/src/main/java/br/com/benquer/app/benquer/LoginActivity.java
@@ -1,14 +1,12 @@
 package br.com.benquer.app.benquer;
 
-import android.content.Context;
-import android.content.SharedPreferences;
+
+import android.content.*;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
+import android.view.*;
+import android.widget.*;
 
 import br.com.benquer.app.benquer.interfaces.OAuth2;
 import br.com.benquer.app.benquer.models.TokenRequest;
@@ -19,7 +17,7 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText email;
     EditText password;
@@ -40,72 +38,78 @@ public class LoginActivity extends AppCompatActivity {
         progressBar.setVisibility(View.GONE);
 
         button = (Button) findViewById(R.id.btn_login);
-        button.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(this);
+
+    }
+
+    public void onClick(View v){
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(getString(R.string.api_url))
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        TokenRequest tokenRequest = new TokenRequest();
+
+        tokenRequest.setUsername(email.getText().toString());
+        tokenRequest.setPassword(password.getText().toString());
+        tokenRequest.setGrant_type(getString(R.string.token_request_grant_type));
+        tokenRequest.setClient_id(getString(R.string.token_request_client_id));
+        tokenRequest.setClient_secret(getString(R.string.token_request_client_secret));
+
+        OAuth2 service = retrofit.create(OAuth2.class);
+
+        Call<TokenResponse> tokenResponseCall = service.getTokenAccess(tokenRequest);
+
+        tokenResponseCall.enqueue(new Callback<TokenResponse>() {
+
             @Override
-            public void onClick(View view) {
+            public void onResponse(Call<TokenResponse> call, Response<TokenResponse> response) {
+                int statusCode = response.code();
 
-                progressBar.setVisibility(View.VISIBLE);
+                TokenResponse tokenResponse = response.body();
 
-                Retrofit retrofit = new Retrofit.Builder()
-                        .baseUrl(getString(R.string.api_url))
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build();
+                Log.d("LoginActivity", "onResponse: " + statusCode);
 
-                TokenRequest tokenRequest = new TokenRequest();
+                if(statusCode == 200) {
 
-                tokenRequest.setUsername(email.getText().toString());
-                tokenRequest.setPassword(password.getText().toString());
-                tokenRequest.setGrant_type(getString(R.string.token_request_grant_type));
-                tokenRequest.setClient_id(getString(R.string.token_request_client_id));
-                tokenRequest.setClient_secret(getString(R.string.token_request_client_secret));
+                    sharedPref = getSharedPreferences("Auth", Context.MODE_PRIVATE);
+                    editor = sharedPref.edit();
 
-                OAuth2 service = retrofit.create(OAuth2.class);
+                    editor.putString("access_token", tokenResponse.getAccess_token());
+                    editor.putString("token_type", tokenResponse.getToken_type());
+                    editor.putString("refresh_token", tokenResponse.getRefresh_token());
+                    editor.putString("access_token", tokenResponse.getAccess_token());
+                    editor.commit();
 
-                Call<TokenResponse> tokenResponseCall = service.getTokenAccess(tokenRequest);
+                }
 
-                tokenResponseCall.enqueue(new Callback<TokenResponse>() {
+                progressBar.setVisibility(View.GONE);
 
-                    @Override
-                    public void onResponse(Call<TokenResponse> call, Response<TokenResponse> response) {
-                        int statusCode = response.code();
-
-                        TokenResponse tokenResponse = response.body();
-
-                        Log.d("LoginActivity", "onResponse: " + statusCode);
-
-                        if(statusCode == 200) {
-
-                            sharedPref = getSharedPreferences("Auth", Context.MODE_PRIVATE);
-                            editor = sharedPref.edit();
-
-                            editor.putString("access_token", tokenResponse.getAccess_token());
-                            editor.putString("token_type", tokenResponse.getToken_type());
-                            editor.putString("refresh_token", tokenResponse.getRefresh_token());
-                            editor.putString("access_token", tokenResponse.getAccess_token());
-                            editor.commit();
-
-                            String access_token = sharedPref.getString("access_token", "");
-                            Log.d("access_token", access_token);
-
-                        }
-
-                        progressBar.setVisibility(View.GONE);
-
-                    }
-
-                    @Override
-                    public void onFailure(Call<TokenResponse> call, Throwable t) {
-
-                        Log.d("LoginActivity", "onFailure: " + t.getMessage());
-                        progressBar.setVisibility(View.GONE);
-
-                    }
-
-                });
+                AccountTypesList();
 
 
             }
+
+            @Override
+            public void onFailure(Call<TokenResponse> call, Throwable t) {
+
+                Log.d("LoginActivity", "onFailure: " + t.getMessage());
+                progressBar.setVisibility(View.GONE);
+
+            }
+
         });
+
+
+    }
+
+    public void AccountTypesList(){
+
+        Intent it = new Intent(this, AccountTypesListActivity.class);
+        startActivity(it);
 
     }
 }
